cdcbca1a0c0a859c2aab6d8f91183dface1d52ee
LazicAleksandar99 <100857692+LazicAleksandar99@users.noreply.github.com>
2023-05-01 11:49:56 +0200

    User model
    Added user class

---
+++ Backend/Shopping.Api/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shopping.Api.Models
+{
+    public enum UserRole { Administrator = 0, Seller = 1, Customer = 2 };
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+    }
+}
