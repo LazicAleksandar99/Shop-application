1184069a989efa8b6ff03f45d8d900dbb640a263
LazicAleksandar99 <100857692+LazicAleksandar99@users.noreply.github.com>
2023-05-01 11:55:06 +0200

    Updated User model

--- Backend/Shopping.Api/Models/User.cs
+++ Backend/Shopping.Api/Models/User.cs
@@ -4,8 +4,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Shopping.Api.Models
 {
     public enum UserRole { Administrator = 0, Seller = 1, Customer = 2 };
+    public enum VerificationStatus { Pending = 0, Denied = 1, Verified = 2};
     public class User
     {
+        [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         [Required]
@@ -16,10 +18,24 @@ namespace Shopping.Api.Models
         public string Email { get; set; } = string.Empty;
         [Required]
         [StringLength(100)]
-        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
         [Required]
         [StringLength(100)]
         public string LastName { get; set; } = string.Empty;
-
+        [Required]
+        public DateTime Birthday { get; set; } 
+        [Required]
+        [StringLength(255)]
+        public string Address { get; set; } = string.Empty;
+        [Required]
+        public UserRole Role { get; set; }
+        [Required]
+        public VerificationStatus Verification { get; set; }
+        [Required]
+        public byte[] Password { get; set; } 
+        [Required]
+        public byte[] PasswordKey { get; set; }
+        public string Picture { get; set; } = string.Empty;
+        
     }
 }
