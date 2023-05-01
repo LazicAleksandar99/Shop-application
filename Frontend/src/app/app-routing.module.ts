75ffb0b058f4a47ded697ada43051ec42b115f1d
LazicAleksandar99 <100857692+LazicAleksandar99@users.noreply.github.com>
2023-05-01 12:36:02 +0200

    Authentication component
    added authentication component, login, and registration.

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/authentication/login',pathMatch:'full'},
+  {
+    path:'user', component: UserComponent,
+    children:[
+      { path: 'login', component: LoginComponent},
+      { path: 'registration', component: RegistrationComponent},
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './pages/authentication/authentication.component';
+import { LoginComponent } from './pages/authentication/login/login.component';
+import { RegistrationComponent } from './pages/authentication/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    LoginComponent,
+    RegistrationComponent,
   ],
   imports: [
     BrowserModule,
